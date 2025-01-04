aa6182a8f646c32d289103612c5987374840a10c
Shubham141102 <shubhamhippargiiiit@gmail.com>
2025-01-04 23:38:56 +0530

    started ll

---
+++ days_3.0/day9_linkedlist.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node(data1,next1)
+    {
+        data = data1;
+        next = next1;
+    }
+};
+int main(){
+    return 0;
+}
